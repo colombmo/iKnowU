ecfbada5f4ffab849a557ea8f5f6f1228809a9e9
Moreno Colombo <moreno.colombo@unifr.ch>
2018-06-03 12:23:03 +0200

    Added user manual and installers

---
+++ iKnowU_Server/soft_install/linux_osx_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip install numpy
+pip install scikit-image
+pip install opencv-python
+pip install pygame
+pip install dlib
+echo installation finished!
---
+++ iKnowU_Server/soft_install/windows_install.bat
@@ -0,0 +1,9 @@
+@echo off
+title iKnowU installation
+pip install numpy
+pip install scikit-image
+pip install opencv-python
+pip install pygame
+pip install dlib
+echo Installation finished!
+pause
