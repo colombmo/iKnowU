0b5cac78ac69398b5a739b73c451217abe0834aa
Moreno Colombo <moreno.colombo@unifr.ch>
2018-06-04 18:16:56 +0200

    Corrected windows installation

--- iKnowU_Server/soft_install/windows_install.bat
+++ iKnowU_Server/soft_install/windows_install.bat
@@ -1,9 +1,10 @@
 @echo off
 title iKnowU installation
 pip install numpy
+pip install scipy
 pip install scikit-image
 pip install opencv-python
 pip install pygame
-pip install dlib
+pip install dlib-19.8.1-cp36-cp36m-win_amd64.whl
 echo Installation finished!
 pause
